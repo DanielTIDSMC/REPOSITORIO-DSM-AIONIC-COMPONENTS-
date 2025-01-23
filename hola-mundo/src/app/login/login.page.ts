861636806d06fad19be091483043476cc8bd3a3d
DanielTIDSMC <joaquingarciacobos3@gmail.com>
2025-01-23 09:00:56 -0600

    implementar formularios

--- hola-mundo/src/app/login/login.page.ts
+++ hola-mundo/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,24 +8,44 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class LoginPage implements OnInit {
-
+  
   email: string = '';
-  password: string = ''; 
+  password: string = '';
 
   constructor() { }
 
   ngOnInit() {
   }
-  
-  login (){
-    if(this.email === 'admin'
-      && this.password === 'admin' ) {
-        console.log('login correcto');
-      }
-      else {
-        console.log('login incorrecto');
-      }
 
+  login(formulario: NgForm) {
+
+    // console.log('email: ', this.email);
+    // console.log('password: ', this.password);
+
+    // Obtener los valores del formulario
+    console.log(formulario.value);
+
+    // Validar formulario
+    console.log('fomr', formulario.value);
+    
+    // Si usuario= admin y password=admin
+    // Entonces console.log('Login correcto')
+    // Sino console.log('Login incorrecto')
+    if(formulario.invalid){
+      console.log('Todos los campos son requeridos');
+      return;
+    }
+
+    if (this.email === 'admin' && this.password === 'admin') {
+      console.log('Login correcto');
+      return;
+    }
+    else {
+      console.log('Login incorrecto');
+    }
   }
 
 }
+
+
+
